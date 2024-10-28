2399a8d637e2e9d1543e77f9737b612ffa8cba26
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-28 13:40:44 +0100

    added variables for the access and secret keys

--- devops/terraform/answers/13_a.tf
+++ devops/terraform/answers/13_a.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "eu-north-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "var.access_key"
+  secret_key = "var.secret_key"
 }
 
 resource "aws_vpc" "main" {
