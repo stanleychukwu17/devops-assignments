d008b7ea55e3dc1802855a8c4ee6e9809717dd14
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-19 10:36:46 +0100

    added the entry script answer for question 36.2

---
+++ devops/terraform/answers/36.2-entry-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y install httpd 
+sudo systemctl start httpd && sudo systemctl enable httpd
+echo "Hello from $(hostname -f), Deployed via Terraform" > /var/www/html/index.html
+
+sudo apt-get -y install docker
+sudo systemctl start docker
+
+sudo usermod -aG docker ec2-user
+
+sudo docker run -d -p 8080:80 nginx
