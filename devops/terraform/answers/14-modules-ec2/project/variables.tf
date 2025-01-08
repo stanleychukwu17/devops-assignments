6b82f7f00cd7805d570e3f8b1977eb4d1c7a562d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-01-08 11:37:19 +0100

    added the variables

---
+++ devops/terraform/answers/14-modules-ec2/project/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "the access key for them man"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "the secret key for them man"
+  type        = string
+  sensitive   = true
+}
