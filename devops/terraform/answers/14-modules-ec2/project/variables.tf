c39d171633007c2b372006a260685fcea4169686
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-01-08 19:32:02 +0100

    added the variables that will be used with the ec2 module imported

--- devops/terraform/answers/14-modules-ec2/project/variables.tf
+++ devops/terraform/answers/14-modules-ec2/project/variables.tf
@@ -1,3 +1,6 @@
+# =====================
+# root module variables
+# =====================
 variable "access_key" {
   description = "the access key for them man"
   type        = string
@@ -8,4 +11,32 @@ variable "secret_key" {
   description = "the secret key for them man"
   type        = string
   sensitive   = true
+}
+
+
+# =====================
+# ec variables
+# =====================
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+  default     = "ami-0c2b8ca1dad456f8a"
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_count" {
+  description = "The number of instances to create"
+  type        = number
+  default     = 2
+}
+
+variable "public_ip" {
+  description = "Assign a public IP address"
+  type        = bool
+  default     = true
 }
