ac6e0137387435b1ded69b6ae8f7f7af0ed00765
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-01-08 11:37:36 +0100

    added the vpc resources

---
+++ devops/terraform/answers/14-modules-ec2/project/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
