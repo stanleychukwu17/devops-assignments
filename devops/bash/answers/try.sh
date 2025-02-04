8cf9e690acb228084dee1c829957ab6298195cb6
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-02-04 09:35:04 +0100

    added the answer to question 9

--- devops/bash/answers/try.sh
+++ devops/bash/answers/try.sh
@@ -1,4 +1,5 @@
-total_1=$((5+7))
-total_2=$((10*10))
-total_3=$((total_1+total_2))
-echo "${total_1} + ${total_2} = $total_3"
+strA="Hello world"
+strB="Hello world"
+if [ "$strA" == "$strB" ]; then
+    echo "all is well"
+fi
