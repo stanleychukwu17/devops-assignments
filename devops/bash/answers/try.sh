af7c9bb68143418cbff3660544e868ba4bab216c
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-02-06 00:27:15 +0100

    added the answer for question. 11

--- devops/bash/answers/try.sh
+++ devops/bash/answers/try.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
-# -----------------------------
-# hello.sh is the file that we will check
-# -----------------------------
-check_file=hello.sh
+# my way
+i=1
+cat names.txt | while read name; do
+    echo "$i $name"
+    ((i++))
+done
 
+echo -e "\n\n from GPT \n\n"
 
-# -----------------------------
-# checks to see if the file exists
-# if it does, we will create the file
-# -----------------------------
-if [ -e "$check_file" ]; then
-    echo "$check_file exists"
-    ls -l "$check_file"
-else
-    echo "$check_file does not exist"
-    touch "$check_file"
-    ls -l "$check_file"
-fi
-
-
-# -----------------------------
-# checks to see if the file is executable
-# if it is not, we will make it executable
-# -----------------------------
-if [ -x "$check_file" ]; then
-    echo "$check_file is executable"
-else
-    echo "$check_file is not executable"
-
-    echo "#!/bin/bash" > "$check_file"
-    echo "echo \"hello world\"" >> "$check_file"
-
-    chmod +x "$check_file"
-
-    ls -l "$check_file"
-fi
+# from chatGPT
+i=1
+while read name; do
+    echo "$i $name"
+    ((i++))
+done < names.txt
