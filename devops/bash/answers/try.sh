8398b411985f71bcae226782d2953980446ce389
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-02-06 08:38:10 +0100

    added questions for for loop

--- devops/bash/answers/try.sh
+++ devops/bash/answers/try.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-
-# my way
-i=1
-cat names.txt | while read name; do
-    echo "$i $name"
-    ((i++))
+# loop through a file
+for i in `cat names.txt`; do
+    echo "$i"
 done
 
 echo -e "\n\n from GPT \n\n"
-
 # from chatGPT
-i=1
-while read name; do
-    echo "$i $name"
-    ((i++))
-done < names.txt
+for i in $(cat names.txt); do
+    echo "$i"
+done
+
+# loop through an array
+arr=(1 2 3 4 5)
+for i in "${arr[@]}"; do
+    echo "$i"
+done
+
+# loop through a string
+str="hello world"
+for i in $str; do
+    echo "$i"
+done
+
+echo ""
+# loop through an argument
+for i in "$@"; do
+    echo "$i"
+done
+
+echo ""
+# loop each argument
+for i in $1 $2 $3; do
+    echo "$i"
+done
