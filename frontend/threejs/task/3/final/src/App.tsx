304d83beed374aeccf061b08f5c494f729f14485
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-06-23 10:03:23 +0100

    cleaned the code up

--- frontend/threejs/task/3/final/src/App.tsx
+++ frontend/threejs/task/3/final/src/App.tsx
@@ -1,54 +1,53 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, SoftShadows, MeshWobbleMaterial } from '@react-three/drei'
-import {useSpring, a} from '@react-spring/three'
 import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { SoftShadows, OrbitControls, MeshWobbleMaterial} from '@react-three/drei'
+import {a, useSpring} from '@react-spring/three'
 import './App.css'
+import { useRef, useState } from 'react'
 
 
-
-type spinProps = {
-  position: [number, number, number],
-  color: string,
-  speed: number,
+type BoxProps = {
+  position: [number, number, number]
   args: ConstructorParameters<typeof THREE.BoxGeometry>
+  color: string
+  hoverColor: string
 }
-const SpinningMesh = ({position, color, speed, args}: spinProps) => {
-  // ref to the mesh
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const [expand, setExpand] = useState(false)
+const Box = ({position, args, color, hoverColor}: BoxProps) => {
+  const boxRef = useRef<THREE.Mesh>(null!)
+  const [active, setActive] = useState<boolean>(false)
+  const [hovered, setHovered] = useState<boolean>(false)
   const props = useSpring({
-    scale: expand ? 1.5 : 1,
+    scale: active ? 1.5 : 1,
   })
 
-  useFrame(() => {
-    meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01
+  useFrame((_state, delta) => {
+    boxRef.current.rotation.x = boxRef.current.rotation.y += delta
   })
 
   return (
     <a.mesh
-      ref={meshRef}
+      ref={boxRef}
       position={position}
-      onClick={() => setExpand(!expand)}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
       scale={props.scale}
       castShadow
     >
-      <boxGeometry args={args} />
-      <meshStandardMaterial color="hotpink" />
+      <boxGeometry args={args}></boxGeometry>
+      <meshStandardMaterial color={color}></meshStandardMaterial>
       <MeshWobbleMaterial
-        color={color}
-        speed={speed}
+        color={hovered? hoverColor : color}
         factor={0.6}
       />
     </a.mesh>
   )
 }
 
-function App() {
-
+function App () {
   return (
     <div className='App'>
-      <div className='three_Canvas'>
+      <div className="three_Canvas">
         <Canvas
           shadows
           camera={{ position: [-5, 2, 10], fov: 60 }}
@@ -63,34 +62,32 @@ function App() {
           <directionalLight
             castShadow
             position={[0, 10, 0]} 
-            intensity={1.5}
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-            shadow-radius={10}
-            shadow-camera-far={50}
-            shadow-camera-left={-10}
-            shadow-camera-right={10}
-            shadow-camera-top={10}
-            shadow-camera-bottom={-10}
+            intensity={Math.PI/3}
+            // shadow-mapSize-width={1024}
+            // shadow-mapSize-height={1024}
+            // shadow-radius={10}
+            // shadow-camera-far={50}
+            // shadow-camera-left={-10}
+            // shadow-camera-right={10}
+            // shadow-camera-top={10}
+            // shadow-camera-bottom={-10}
           />
-
           {/* both pointLights will help to illuminate the spinning boxes */}
           <pointLight position={[-10, 0, -20]} intensity={0.5} />
           <pointLight position={[0, -10, 0]} intensity={1.5} />
 
           <group>
             <mesh
-              rotation={[-Math.PI / 2, 0, 0]}
+              rotation={[-Math.PI/2, 0, 0]}
               position={[0, -3, 0]}
               receiveShadow
             >
               <planeGeometry args={[100, 100]} />
-              <shadowMaterial attach="material" opacity={0.3} />
+              <shadowMaterial attach="material" opacity={1.3} />
             </mesh>
 
-            <SpinningMesh position={[0, 0, 0]} color="hotpink" speed={2} args={[2, 3, 2]} />
-            <SpinningMesh position={[-5, 0, -1]} color="yellowgreen" speed={2} args={[2, 3, 2]} />
-            <SpinningMesh position={[5, 0, 3]} color="purple" speed={2} args={[2, 4, 2]} />
+            <Box position={[2,0,0]} args={[1,2,1.5]} color='hotpink' hoverColor='yellow' />
+            <Box position={[-2,0,0]} args={[2,1.5,1]} color='purple' hoverColor='yellow' />
           </group>
 
           <OrbitControls />
