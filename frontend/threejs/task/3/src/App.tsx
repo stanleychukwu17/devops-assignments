dddf98fba286c84575e227fc3d548ca08a9aec46
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-06-24 13:49:19 +0100

    cleaned up the App.tsx

--- frontend/threejs/task/3/src/App.tsx
+++ frontend/threejs/task/3/src/App.tsx
@@ -11,8 +11,9 @@ type BoxProps = {
   args: ConstructorParameters<typeof THREE.BoxGeometry>
   color: string
   hoverColor: string
+  speed: number
 }
-const Box = ({position, args, color, hoverColor}: BoxProps) => {
+const Box = ({position, args, color, hoverColor, speed}: BoxProps) => {
   const boxRef = useRef<THREE.Mesh>(null!)
   const [active, setActive] = useState<boolean>(false)
   const [hovered, setHovered] = useState<boolean>(false)
@@ -34,10 +35,11 @@ const Box = ({position, args, color, hoverColor}: BoxProps) => {
       scale={props.scale}
       castShadow
     >
-      <boxGeometry args={args}></boxGeometry>
+      <boxGeometry args={args} />
       <meshStandardMaterial color={color}></meshStandardMaterial>
       <MeshWobbleMaterial
         color={hovered? hoverColor : color}
+        speed={speed}
         factor={0.6}
       />
     </a.mesh>
@@ -50,7 +52,7 @@ function App () {
       <div className="three_Canvas">
         <Canvas
           shadows
-          camera={{ position: [-5, 2, 10], fov: 60 }}
+          camera={{ position: [-5, 2, 10], fov: 80 }}
         >
           {/* Enable soft shadows in the scene */}
           <SoftShadows />
@@ -63,14 +65,14 @@ function App () {
             castShadow
             position={[0, 10, 0]} 
             intensity={Math.PI/3}
-            // shadow-mapSize-width={1024}
-            // shadow-mapSize-height={1024}
-            // shadow-radius={10}
-            // shadow-camera-far={50}
-            // shadow-camera-left={-10}
-            // shadow-camera-right={10}
-            // shadow-camera-top={10}
-            // shadow-camera-bottom={-10}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-radius={10}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
           />
           {/* both pointLights will help to illuminate the spinning boxes */}
           <pointLight position={[-10, 0, -20]} intensity={0.5} />
@@ -83,11 +85,12 @@ function App () {
               receiveShadow
             >
               <planeGeometry args={[100, 100]} />
-              <shadowMaterial attach="material" opacity={1.3} />
+              <shadowMaterial attach="material" opacity={0.3} />
             </mesh>
 
-            <Box position={[2,0,0]} args={[1,2,1.5]} color='hotpink' hoverColor='yellow' />
-            <Box position={[-2,0,0]} args={[2,1.5,1]} color='purple' hoverColor='yellow' />
+            <Box position={[0,0,0]} args={[2,3,1.5]} color='hotpink' hoverColor='yellow' speed={2} />
+            <Box position={[-5,0,-1]} args={[2,3,1.5]} color='purple' hoverColor='yellow' speed={2} />
+            <Box position={[5,0,3]} args={[2,3,1.5]} color='yellowgreen' hoverColor='yellow' speed={2} />
           </group>
 
           <OrbitControls />
