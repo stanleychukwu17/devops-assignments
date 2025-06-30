b04e173f2c8b3828029bb3f0fe4aa6040698cb11
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-06-30 07:27:57 +0100

    moved the App.css to the main css file, removed the class .App from this component and moved it to the root App.tsx

--- frontend/threejs/task/3/src/Task3.tsx
+++ frontend/threejs/task/3/src/Task3.tsx
@@ -3,7 +3,6 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { SoftShadows, OrbitControls, MeshWobbleMaterial} from '@react-three/drei'
 import {a, useSpring} from '@react-spring/three'
 import { useRef, useState } from 'react'
-import './App.css'
 
 
 type BoxProps = {
@@ -46,58 +45,56 @@ const Box = ({position, args, color, hoverColor, speed}: BoxProps) => {
   )
 }
 
-function App () {
+function Task3 () {
   return (
-    <div className='App'>
-      <div className="three_Canvas">
-        <Canvas
-          shadows
-          camera={{ position: [-5, 2, 10], fov: 80 }}
-        >
-          {/* Enable soft shadows in the scene */}
-          <SoftShadows />
+    <div className="three_Canvas">
+      <Canvas
+        shadows
+        camera={{ position: [-5, 2, 10], fov: 80 }}
+      >
+        {/* Enable soft shadows in the scene */}
+        <SoftShadows />
 
-          {/* this light makes things to look pretty */}
-          <ambientLight intensity={0.3} />
+        {/* this light makes things to look pretty */}
+        <ambientLight intensity={0.3} />
 
-          {/* our main source of light, also casting the shadows */}
-          <directionalLight
-            castShadow
-            position={[0, 10, 0]} 
-            intensity={Math.PI/3}
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-            shadow-radius={10}
-            shadow-camera-far={50}
-            shadow-camera-left={-10}
-            shadow-camera-right={10}
-            shadow-camera-top={10}
-            shadow-camera-bottom={-10}
-          />
-          {/* both pointLights will help to illuminate the spinning boxes */}
-          <pointLight position={[-10, 0, -20]} intensity={0.5} />
-          <pointLight position={[0, -10, 0]} intensity={1.5} />
+        {/* our main source of light, also casting the shadows */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]} 
+          intensity={Math.PI/3}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-radius={10}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* both pointLights will help to illuminate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
 
-          <group>
-            <mesh
-              rotation={[-Math.PI/2, 0, 0]}
-              position={[0, -3, 0]}
-              receiveShadow
-            >
-              <planeGeometry args={[100, 100]} />
-              <shadowMaterial attach="material" opacity={0.3} />
-            </mesh>
+        <group>
+          <mesh
+            rotation={[-Math.PI/2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow
+          >
+            <planeGeometry args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
 
-            <Box position={[0,0,0]} args={[2,3,1.5]} color='hotpink' hoverColor='yellow' speed={2} />
-            <Box position={[-5,0,-1]} args={[2,3,1.5]} color='purple' hoverColor='yellow' speed={2} />
-            <Box position={[5,0,3]} args={[2,3,1.5]} color='yellowgreen' hoverColor='yellow' speed={2} />
-          </group>
+          <Box position={[0,0,0]} args={[2,3,1.5]} color='hotpink' hoverColor='yellow' speed={2} />
+          <Box position={[-5,0,-1]} args={[2,3,1.5]} color='purple' hoverColor='yellow' speed={2} />
+          <Box position={[5,0,3]} args={[2,3,1.5]} color='yellowgreen' hoverColor='yellow' speed={2} />
+        </group>
 
-          <OrbitControls />
-        </Canvas>
-      </div>
+        <OrbitControls />
+      </Canvas>
     </div>
   )
 }
 
-export default App
+export default Task3
