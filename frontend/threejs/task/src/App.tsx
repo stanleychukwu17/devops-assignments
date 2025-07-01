dd0dfe21f33203576d7dc86caca37677e04a6240
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-01 05:08:22 +0100

    now displaying task 7

--- frontend/threejs/task/src/App.tsx
+++ frontend/threejs/task/src/App.tsx
@@ -1,12 +1,13 @@
 // import Task3 from "./components/Task3/Task3"
-import Task6 from "./components/Task6/Task6"
+// import Task6 from "./components/Task6/Task6"
+import Task7 from "./components/Task7/Task7"
 import './App.css'
 
 
 export default function App() {
   return (
     <div className="App">
-      <Task6 />
+      <Task7 />
     </div>
   )
 }
