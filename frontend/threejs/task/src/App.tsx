7839c6abe53bee9aafbd2d4c7f50684f480b756b
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-02 09:15:42 +0100

    displaying the test code

--- frontend/threejs/task/src/App.tsx
+++ frontend/threejs/task/src/App.tsx
@@ -1,14 +1,15 @@
+import Task0 from "./components/Task0/Task0"
 // import Task3 from "./components/Task3/Task3"
 // import Task6 from "./components/Task6/Task6"
 // import Task7 from "./components/Task7/Task7"
-import Task8 from "./components/Task8/Task8"
+// import Task8 from "./components/Task8/Task8"
 import './App.css'
 
 
 export default function App() {
   return (
     <div className="App">
-      <Task8 />
+      <Task0 />
     </div>
   )
 }
