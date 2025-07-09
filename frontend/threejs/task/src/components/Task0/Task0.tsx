c6a0842bdc6b66a994582d47821a46f2157f7936
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-09 16:36:14 +0100

    finished the test app

--- frontend/threejs/task/src/components/Task0/Task0.tsx
+++ frontend/threejs/task/src/components/Task0/Task0.tsx
@@ -1,26 +1,27 @@
 import * as THREE from 'three'
-import { Canvas, useFrame } from "@react-three/fiber"
-import { SoftShadows, MeshWobbleMaterial, OrbitControls } from "@react-three/drei"
+import { Canvas, useFrame, } from "@react-three/fiber"
+import { MeshWobbleMaterial, SoftShadows, useCursor, OrbitControls } from '@react-three/drei'
 import { useRef, useState } from "react"
-import { a, useSpring } from '@react-spring/three'
+import { useSpring, a } from '@react-spring/three'
 
 
-type BoxProps = {
-  color: string,
-  hovColor: string,
-  position: [number, number, number],
+type CubeProps = {
+  position: [number, number, number]
   args: [number, number, number]
+  color: string
 }
-const BoxComp = ({color, hovColor, position, args}: BoxProps) => {
+const Cube = ({position, color, args}: CubeProps) => {
   const meshRef = useRef<THREE.Mesh>(null!)
-  const [active, setActive] = useState<boolean>(false)
   const [hovered, setHovered] = useState<boolean>(false)
+  const [active, setActive] = useState<boolean>(false)
   const animate = useSpring({
-    scale: active ? 1.5 : 1
+    scale: active ? 1.3 : 1
   })
 
+  useCursor(true)
+
   useFrame((_state, delta) => {
-    return meshRef.current.rotation.x = meshRef.current.rotation.y += delta
+    meshRef.current.rotation.x = meshRef.current.rotation.y += delta * 0.8
   })
 
   return (
@@ -28,15 +29,15 @@ const BoxComp = ({color, hovColor, position, args}: BoxProps) => {
       ref={meshRef}
       position={position}
       castShadow
-      scale={animate.scale}
       onClick={() => setActive(!active)}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
+      onPointerOver={()=> setHovered(true)}
+      onPointerOut={()=> setHovered(false)}
+      scale={animate.scale}
     >
       <boxGeometry args={args} />
       <MeshWobbleMaterial
-        color={hovered ? hovColor : color}
-        factor={0.8}
+        color={hovered ? "indigo" : color}
+        factor={0.6}
         speed={1.2}
       />
     </a.mesh>
@@ -48,53 +49,38 @@ export default function Task0() {
     <div className="three_Canvas">
       <Canvas
         shadows
-        camera={{ position:[0, 2,10], fov: 60 }}
+        camera={{position:[0, 1, 5], fov:70}}
       >
+
+        {/* for the soft shadow in the scene */}
         <SoftShadows />
 
+        {/* add some ambient light */}
         <ambientLight intensity={0.5} />
 
-        <directionalLight
-          position={[2, 5, 2]}
-          intensity={Math.PI}
-          castShadow
-          shadow-mapSize-width={2096}
-          shadow-mapSize-height={2096}
-        />
+        {/* add some directional Light */}
+        <directionalLight intensity={Math.PI} position={[2,5,2]} castShadow />
+        <directionalLight intensity={Math.PI} position={[-2,5,2]} castShadow />
+
+        <mesh
+          receiveShadow
+          position={[0,-3,0]}
+          rotation={[-Math.PI/2, 0, 0]}
+        >
+          <planeGeometry args={[100,100]} />
+          {/* <meshStandardMaterial color={"indigo"} /> */}
+          <shadowMaterial attach={"material"} opacity={0.8} />
+        </mesh>
 
         <group>
-          <mesh
-            rotation={[-Math.PI/2,0,0]}
-            position={[0,-4,0]}
-            receiveShadow
-          >
-            <planeGeometry args={[100,100]} />
-            <shadowMaterial attach={"material"} opacity={0.7} />
-          </mesh>
-
-          <BoxComp
-            position={[-5, 0, 0]}
-            color="purple"
-            hovColor="indigo"
-            args={[2,2.5,1.5]}
-          />
-          <BoxComp
-            position={[0, 0, 0]}
-            color="hotpink"
-            hovColor="yellowgreen"
-            args={[2,2.5,1.5]}
-          />
-          <BoxComp
-            position={[5, 0, 0]}
-            color="#61b79a"
-            hovColor="#1b5c50"
-            args={[2,2.5,1.5]}
-          />
+          <Cube color='#443cf8' position={[2.5,0,0]} args={[1.2,1.8,.8]} />
+          <Cube color='#dc10a9' position={[0,0,0]} args={[1.2,1.8,.8]} />
+          <Cube color='#188cc1' position={[-2.5,0,0]} args={[1.2,1.8,.8]} />
         </group>
 
         <OrbitControls
-          minPolarAngle={0}                  // look horizontally
-          maxPolarAngle={Math.PI / 2}        // limit to top hemisphere
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI/2}
         />
       </Canvas>
     </div>
