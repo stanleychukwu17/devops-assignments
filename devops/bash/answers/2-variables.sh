42e28e23be0c8e7f257277dd89aac98804150f9a
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-08-01 09:53:58 +0100

    update: added the new answer

--- devops/bash/answers/2-variables.sh
+++ devops/bash/answers/2-variables.sh
@@ -3,11 +3,12 @@
 greet=hello
 echo $greet
 
-welcome="Hello world, bash rules your terminal"
+welcome="bash rules your terminal, use bash today"
 echo ${welcome}
 
 echo ""
 echo ${welcome/bash/makefile}
+echo ${welcome//bash/golang}
 
 echo ""
 echo "sliced: ${welcome::11}"
