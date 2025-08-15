fce25f4d79f57a7ac8a382e77e254477f1b365c1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-08-15 11:19:06 +0100

    re-wrote the entire code for practice

--- frontend/threejs/task/src/components/Task0/Task0.tsx
+++ frontend/threejs/task/src/components/Task0/Task0.tsx
@@ -1,19 +1,18 @@
-import { useRef, useState } from 'react';
 import * as THREE from 'three'
-import { Canvas, useFrame } from "@react-three/fiber";
-import {SoftShadows, MeshWobbleMaterial, OrbitControls} from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { SoftShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei'
+import { useRef, useState } from 'react'
 import {a, useSpring} from '@react-spring/three'
 
-type BoxProp = {
-  color: string
-  hoverColor: string
-  args: [number, number, number]
+type BoxProps = {
   position: [number, number, number]
+  args: [number, number, number]
+  color: string
 }
-const BoxComp = ({position, args, color, hoverColor}: BoxProp) => {
+const Box = ({color, position, args}: BoxProps) => {
   const boxRef = useRef<THREE.Mesh>(null!)
-  const [hovered, setHovered] = useState<boolean>(false)
-  const [active, setActive] = useState<boolean>(false)
+  const [active, setActive] = useState(false)
+  const [hovered, setHovered] = useState(false)
   const props = useSpring({
     scale: active ? 1.2 : 1
   })
@@ -22,61 +21,52 @@ const BoxComp = ({position, args, color, hoverColor}: BoxProp) => {
     boxRef.current.rotation.x = boxRef.current.rotation.y += delta
   })
 
-  return(
+  return (
     <a.mesh
       ref={boxRef}
       position={position}
       castShadow
-      scale={props.scale}
       onClick={() => setActive(!active)}
-      onPointerOver={()=>setHovered(true)}
-      onPointerOut={()=>setHovered(false)}
+      onPointerOver={() => setHovered(true)}
+      onPointerLeave={() => setHovered(false)}
+      scale={props.scale}
     >
-      <boxGeometry args={[...args,10,10,10]} />
+      <boxGeometry args={[...args, 10, 10, 10]} />
       <MeshWobbleMaterial
-        color={hovered ? hoverColor : color}
-        factor={1.8} speed={1}
+        color={ hovered ? 'indigo' : color} speed={1} factor={0.6}
+
       />
     </a.mesh>
   )
 }
 
 export default function Task0() {
-  const y = 1.5, x = .9, z = 1;
-
   return (
     <div className="three_Canvas">
       <Canvas
         shadows
-        camera={{position:[-5,2,10], fov:70}}
+        camera={{ position: [0, 0, 9], fov: 80 }}
       >
+        <ambientLight intensity={Math.PI/2} />
+        <directionalLight intensity={Math.PI/2} position={[2,5,1]} castShadow />
+        <directionalLight intensity={Math.PI/2} position={[-3,5,-2]} />
+
         <SoftShadows />
 
-        <ambientLight intensity={Math.PI/2} />
-
-        <directionalLight
-          position={[3,10,2]}
-          intensity={Math.PI/2}
-          castShadow
-        />
-        <directionalLight
-          position={[-5,0,5]}
-          intensity={1.5}
-        />
-
+        <mesh
+          position={[0,-3,0]}
+          receiveShadow
+          rotation={[-Math.PI/2,0,0]}
+        >
+          <planeGeometry args={[100,100]} />
+          {/* <meshStandardMaterial color={"lightgreen"} /> */}
+          <shadowMaterial attach="material" opacity={0.6} />
+        </mesh>
 
         <group>
-          <mesh
-            receiveShadow
-            rotation={[-Math.PI/2, 0, 0]}
-            position={[0,-3,0]}
-          >
-            <planeGeometry args={[100,100]} />
-            <shadowMaterial attach={"material"} opacity={.8} />
-          </mesh>
-          <BoxComp position={[0,0,0]} args={[x,y,z]} color='#8cdc1b' hoverColor='white' />
-          <BoxComp position={[3,0,0]} args={[x,y,z]} color='#c713e7' hoverColor='white' />
-          <BoxComp position={[-3,0,0]} args={[x,y,z]} color='#b51f1c' hoverColor='white' />
+          <Box color='#d505cf' position={[-3,0,0]} args={[1.2,1.7,1.2]}/>
+          <Box color='#26e3d6' position={[0,0,0]} args={[1.2,1.7,1.2]}/>
+          <Box color='#6f58f6' position={[3,0,0]} args={[1.2,1.7,1.2]}/>
         </group>
 
         <OrbitControls
