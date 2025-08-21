bd28743f3c850b442649955d84e517992dddc238
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-08-21 20:39:37 +0100

    updated the example

--- frontend/threejs/task/src/components/Task3/z-example2.tsx
+++ frontend/threejs/task/src/components/Task3/z-example2.tsx
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { Canvas, useFrame } from "@react-three/fiber"
-import { SoftShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei'
+import { SoftShadows, MeshWobbleMaterial, OrbitControls, useHelper } from '@react-three/drei'
 import { useRef, useState } from "react"
 import { useSpring, a } from '@react-spring/three'
+import { useControls } from 'leva'
 
 
 type BoxProps = {
@@ -22,6 +23,15 @@ const Box = ({position, args, color}: BoxProps) => {
     meshRef.current.rotation.x = meshRef.current.rotation.y += delta
   })
 
+  useHelper(meshRef, THREE.BoxHelper, "red")
+
+  const {boxColor, xSize, ySize, zSize} = useControls(`box-${color}`, {
+    boxColor: color,
+    xSize: {value: args[0], min: 0.5, max: 5, step: 0.25},
+    ySize: {value: args[1], min: 0.5, max: 5, step: 0.25},
+    zSize: {value: args[2], min: 0.5, max: 5, step: 0.25},
+  })
+
   return (
     <a.mesh
       position={position}
@@ -32,13 +42,43 @@ const Box = ({position, args, color}: BoxProps) => {
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
     >
-      <boxGeometry args={args} />
+      <boxGeometry args={[xSize, ySize, zSize]} />
       <MeshWobbleMaterial
-        color={hovered ? "#295e4f" : color }
+        color={hovered ? "#295e4f" : boxColor }
         factor={0.8}
         speed={1.8}
       />
     </a.mesh>
+  )
+}
+
+type DirectLightProps = {
+  position: [number, number, number]
+  intensity: number
+  shadow: boolean
+  color: string
+}
+const DirectLight = ({position, intensity, shadow, color}: DirectLightProps) => {
+  const lightRef = useRef<THREE.Mesh>(null!)
+
+  const {lightIntensity, lightColor} = useControls(`${color}-light`, {
+    lightIntensity: {
+      value: intensity,
+      min: 1,
+      max: 15,
+      step: 0.5
+    },
+    lightColor: {
+      value: color
+    }
+  })
+  useHelper(lightRef, THREE.DirectionalLightHelper, 1)
+
+  return (
+    <directionalLight
+      color={lightColor}
+      ref={lightRef} position={position} intensity={lightIntensity} castShadow={shadow}
+    />
   )
 }
 
@@ -50,11 +90,14 @@ export default function Task0() {
         camera={{ position: [0,3,10], fov: 70 }}
         shadows
       >
+        <axesHelper args={[5]} />
+        <gridHelper position={[0, -0.5, 0]} args={[15, 15, 'orange', 'orange']} />
+
         <SoftShadows />
 
         <ambientLight intensity={Math.PI/2} />
-        <directionalLight position={[2,8,0]} intensity={Math.PI/2} castShadow />
-        <directionalLight position={[-5,5,0]} intensity={Math.PI/1.3} />
+        <DirectLight color='#ff9b35' position={[2,8,0]} intensity={Math.PI/2} shadow/>
+        <DirectLight color='#d152af' position={[-5,5,0]} intensity={Math.PI/1.3} shadow={false}  />
 
         <mesh
           rotation={[-Math.PI/2, 0, 0]}
@@ -62,7 +105,7 @@ export default function Task0() {
           receiveShadow
         >
           <planeGeometry args={[100,100]} />
-            <shadowMaterial attach="material" opacity={0.7} />
+          <shadowMaterial attach="material" opacity={0.7} />
         </mesh>
 
         <group>
